c1420f86719c61cf45705053f8f11749dcfd06f1
air-fire <4899102@qq.com>
2026-01-27 20:37:39 +0800

    init

---
+++ main.py
@@ -0,0 +1,47 @@
+import baostock as bs
+import pandas as pd
+from stock.stock import Stock
+import os
+
+
+# 证券类型，其中1：股票，2：指数，3：其它，4：可转债，5：ETF
+
+
+def update_stock_data():
+    bs.login()
+    rs = bs.query_stock_basic()
+    bs.logout()
+    data = []
+    while (rs.error_code == '0') & rs.next():
+        data.append(rs.get_row_data())
+    df = pd.DataFrame(data, columns=rs.fields)
+    df.to_csv("data/stock_data.csv", index=False, encoding="utf_8_sig")
+    print(df)
+
+
+def update_individual_stock_data():
+    stock_df = pd.read_csv("data/stock_data.csv", dtype={'code': str})
+    bs.login()
+    for _, row in stock_df.iterrows():
+        if str(row['type']) == '1' and str(row['status']) == '1' and (str(row['code'])[3:5] not in ['300', '310', '688']):  # 仅更新股票且在市状态的证券
+            stock = Stock()
+            stock.set_symbol(row['code'])
+            stock.save_to_csv()
+            # break  # 测试时只更新一只股票，正式使用时删除此行
+    bs.logout()
+
+
+def main():
+    is_update_stock_data = input("是否更新股票列表数据？(y/n): ").strip().lower() == 'y'
+    if is_update_stock_data:
+        if os.path.exists("data/stock_data.csv"):
+            os.remove("data/stock_data.csv")
+        update_stock_data()
+    else:
+        # 更新个股数据
+        print("更新个股数据。")
+        update_individual_stock_data()
+
+
+if __name__ == "__main__":
+    main()
---
+++ stock/stock.py
@@ -0,0 +1,95 @@
+import baostock as bs
+import pandas as pd
+import os
+import datetime
+
+
+class Stock:
+    def __init__(self):
+        self.symbol = None
+
+    def set_symbol(self, symbol):
+        self.symbol = symbol
+
+    def get_symbol(self):
+        return self.symbol
+
+    def get_start_date(self):
+        if os.path.exists(f'data/{self.get_symbol()}.csv'):
+            df = pd.read_csv(f'data/{self.get_symbol()}.csv')
+            if not df.empty:
+                start_date = pd.to_datetime(df['date'].iloc[-1]) + datetime.timedelta(
+                    days=1)
+                # print(pd.to_datetime(df['date'].iloc[-1]))
+                if start_date >= pd.to_datetime('today', format='%Y-%m-%d'):
+                    return pd.to_datetime('today').date().strftime('%Y-%m-%d')
+                else:
+                    return start_date.strftime('%Y-%m-%d')
+            else:
+                return '2026-01-01'
+        else:
+            return '2026-01-01'
+
+    def get_end_date(self):
+        datetime_today = datetime.date.today()
+        return datetime_today.strftime('%Y-%m-%d')
+
+    def fetch_data(self):
+        if not self.symbol:
+            raise ValueError("Stock symbol is not set.")
+        if self.get_start_date() >= self.get_end_date():
+            raise ValueError("Start date must be earlier than end date.")
+        else:
+            rs = bs.query_history_k_data_plus(code=self.symbol, start_date=self.get_start_date(), end_date=self.get_end_date(
+            ), fields="date,code,open,high,low,close,preclose,volume,amount,adjustflag,turn,tradestatus,pctChg,isST")
+            if rs.error_code != '0':
+                raise ValueError("Failed to fetch stock data.")
+            else:
+                return rs
+
+    def to_dataframe(self):
+        rs = self.fetch_data()
+        data_list = []
+        while rs.next():
+            data_list.append(rs.get_row_data())
+        if not data_list:
+            return pd.DataFrame(columns=rs.fields)
+        return pd.DataFrame(data_list, columns=rs.fields)
+
+    # 判断股票数据文件是否存在且非空
+    def is_data_file_valid(self):
+        if os.path.exists(f'data/{self.get_symbol()}.csv'):
+            df = pd.read_csv(f'data/{self.get_symbol()}.csv')
+            return not df.empty
+        return False
+
+    def save_to_csv(self):
+        df = self.to_dataframe()
+        if df.empty:
+            print("No new data to save.")
+            return
+        else:
+            if self.is_data_file_valid():
+                df.to_csv(f'data/{self.get_symbol()}.csv',
+                          index=False, mode='a', header=False)
+                print(f"Data appended to data/{self.get_symbol()}.csv")
+            else:
+                df.to_csv(f'data/{self.get_symbol()}.csv',
+                          index=False, mode='a')
+                print(f"Data saved to data/{self.get_symbol()}.csv")
+
+    def stock_strategy(self):
+        df = pd.read_csv(f'data/{self.get_symbol()}.csv')
+        df_month = df.tail(30)
+
+        if df['volume'].tail(1) == df['volume'].min():
+            return self.get_symbol
+
+
+if __name__ == "__main__":
+
+    stock = Stock()
+    stock.set_symbol("sh.601919")  # Example stock symbol
+    bs.login()
+    stock.save_to_csv()
+    bs.logout()
